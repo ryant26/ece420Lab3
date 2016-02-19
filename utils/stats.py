563c57257267f4c578376b4ac9688ba8d9c7b607
ryant26 <ryant26@gmail.com>
2016-02-19 12:42:04 -0700

    updated gitignore, inital xls stuff

--- utils/stats.py
+++ utils/stats.py
@@ -1,3 +1,12 @@
-import jdcal
-import openpyxl
+from openpyxl import Workbook
 
+wb = Workbook()
+ws = wb.active
+
+columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+headers = ['Threads', 'Run 1', 'Run 2', 'Run 3', 'Run 4', 'Run 5', 'Average']
+
+for i in range(7):
+	ws[columns[i]+'1'] = headers[i]
+
+wb.save("tst.xlsx")
